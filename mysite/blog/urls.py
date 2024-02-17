fc82c631a294e44d106b03b87c4e1f8a4f2f8de9
HansNijland <hans.nijland@gmail.com>
2024-02-17 20:28:29 +0100

    cleanup

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns =[
     path('',views.post_list, name='post_list'),
-    path('',views.Voetbalspeler_list, name='post_list'),
-    path('',views.wielrenner_list, name='wielrenner_list'),
-
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -10,11 +10,6 @@ from .models import Voetbalspeler
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts': posts})
-def wielrenner_list(request):
-    wielrenners = Wielrenner.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/wielrenner_list.html',{'wielrenners': wielrenners})
-def Voetbalspeler_list(request): 
-    voetbalspelers = Voetbalspeler.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/Voetbalspeler_list.html',{'voetbalspelers': voetbalspelers})
+
 
 
