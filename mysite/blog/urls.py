5b2ba18ead84c142a9e9e23929fd79900e40d64f
HansNijland <hans.nijland@gmail.com>
2024-02-17 19:22:34 +0100

    added sites

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns =[
     path('',views.post_list, name='post_list'),
+    path('',views.Voetbalspeler_list, name='post_list'),
+    path('',views.wielrenner_list, name='wielrenner_list'),
+
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .models import Wielrenner 
+from .models import Voetbalspeler
+
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts': posts})
+def wielrenner_list(request):
+    wielrenners = Wielrenner.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/wielrenner_list.html',{'wielrenners': wielrenners})
+def Voetbalspeler_list(request): 
+    voetbalspelers = Voetbalspeler.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/Voetbalspeler_list.html',{'voetbalspelers': voetbalspelers})
+
+
